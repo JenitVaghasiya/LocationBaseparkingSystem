ee7d8bb53a6e922ed389b8f27427ecb66ad860b2
jzr <jzr@BRDPINCA>
2018-07-21 10:45:19 +0530

    model added

---
+++ LocationBaseparkingSystem/Models/ParkOnVendor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocationBaseparkingSystem.Models
+{
+    public class ParkOnVendor
+    {
+        public int ID { get; set; }
+
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string Long { get; set; }
+
+        public decimal Lat { get; set; }
+
+        public decimal Address { get; set; }
+
+        public string LandMark { get; set; }
+
+        public int  NoOfParkingSpace{ get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public int HourRate { get; set; }
+
+        public int Area { get; set; }
+        public int Email { get; set; }
+    }
+}
---
+++ LocationBaseparkingSystem/Models/ParkOnVendorTrans.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocationBaseparkingSystem.Models
+{
+    public class ParkOnVendorTrans
+    {
+        public int ID { get; set; }
+        public int VenderID { get; set; }
+        public DateTime EntryTime { get; set; }
+        public DateTime ExitTime { get; set; }
+        public DateTime CarNo { get; set; }
+        public bool IsOut { get; set; }
+        public int Hours { get; set; }
+
+        public int Cost { get; set; }
+
+        public string CustomerName { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
