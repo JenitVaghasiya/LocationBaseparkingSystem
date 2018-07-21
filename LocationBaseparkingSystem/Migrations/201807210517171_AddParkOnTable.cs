810f908b45d2bd8680126d17a046a5ebc3996e21
JenitV <jenit>
2018-07-21 11:06:07 +0530

    added new migration

--- LocationBaseparkingSystem/Migrations/201807210517171_AddParkOnTable.cs
+++ LocationBaseparkingSystem/Migrations/201807210517171_AddParkOnTable.cs
@@ -15,23 +15,45 @@ namespace LocationBaseparkingSystem.Migrations
                    UserId = c.String(nullable: false, maxLength: 128),
                    Email = c.String(nullable: false, maxLength: 200),
                    Name = c.String(nullable: false, maxLength: 200),
-                   Longitude = c.Decimal(nullable: true, precision: 4, scale: 10),
-                   Latitude = c.Decimal(nullable: true, precision: 4, scale: 10),
+                   Longitude = c.Decimal(nullable: true, precision: 18, scale: 2),
+                   Latitude = c.Decimal(nullable: true, precision: 18, scale: 2),
                    Address = c.String(nullable: true, maxLength: 400),
                    Landmark = c.String(nullable: true, maxLength: 400),
                    NoOfParkingSpace = c.Int(nullable: false),
                    CreatedDate = c.DateTime(),
                    IsActive = c.Boolean(nullable: false, defaultValue: false),
-                   HourRate = c.Decimal(nullable: true, precision: 4, scale: 10),
+                   HourRate = c.Decimal(nullable: true, precision: 18, scale: 2),
                    Area = c.String(nullable: true, maxLength: 400)
                })
                .PrimaryKey(t => new { t.Id })
-               .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
-               .Index(t => t.UserId);
+               .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true);
+
+            CreateTable(
+               "dbo.ParkOnVendorTrans",
+               c => new
+               {
+                   Id = c.Int(nullable: false, identity: true),
+                   VenderID = c.Int(nullable: false),
+                   CustomerName = c.String(nullable: false, maxLength: 200),
+                   PhoneNo = c.String(nullable: false, maxLength: 200),
+                   CarNo = c.String(nullable: false, maxLength: 200),
+                   EntryTime = c.DateTime(defaultValue: DateTime.Now),
+                   ExitTime = c.DateTime(nullable: true),
+                   IsOut = c.Boolean(nullable: false,defaultValue: false),
+                   TotalHours = c.Decimal(nullable: true),
+                   TotalCost = c.Decimal(nullable: true)
+               })
+               .PrimaryKey(t => new { t.Id })
+               .ForeignKey("dbo.ParkOnVendor", t => t.VenderID, cascadeDelete: true);
         }
 
         public override void Down()
         {
+            DropForeignKey("dbo.ParkOnVendor", "UserId", "dbo.AspNetUsers");
+            DropTable("dbo.ParkOnVendor");
+
+            DropForeignKey("dbo.ParkOnVendorTrans", "VenderID", "dbo.ParkOnVendor");
+            DropTable("dbo.ParkOnVendorTrans");
         }
     }
 }
--- LocationBaseparkingSystem/Models/ParkOnVendorTrans.cs
+++ LocationBaseparkingSystem/Models/ParkOnVendorTrans.cs
@@ -7,17 +7,24 @@ namespace LocationBaseparkingSystem.Models
 {
     public class ParkOnVendorTrans
     {
-        public int ID { get; set; }
-        public int VenderID { get; set; }
-        public DateTime EntryTime { get; set; }
-        public DateTime ExitTime { get; set; }
-        public DateTime CarNo { get; set; }
-        public bool IsOut { get; set; }
-        public int Hours { get; set; }
+        public int Id { get; set; }
 
-        public int Cost { get; set; }
+        public int VenderID { get; set; }
 
         public string CustomerName { get; set; }
+
         public string PhoneNo { get; set; }
+
+        public string CarNo { get; set; }
+
+        public DateTime EntryTime { get; set; }
+
+        public DateTime? ExitTime { get; set; }
+
+        public bool IsOut { get; set; }
+
+        public decimal? TotalHours { get; set; }
+
+        public decimal? TotalCost { get; set; }
     }
 }
