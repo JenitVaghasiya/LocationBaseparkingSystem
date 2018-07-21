59be18e015830d99c9d330b762e5fd4c24a5fb01
JenitV <jenit>
2018-07-21 10:24:37 +0530

    update db connection string

--- LocationBaseparkingSystem/Models/IdentityModels.cs
+++ LocationBaseparkingSystem/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace LocationBaseparkingSystem.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("LocationDBConnection", throwIfV1Schema: false)
         {
         }
 
