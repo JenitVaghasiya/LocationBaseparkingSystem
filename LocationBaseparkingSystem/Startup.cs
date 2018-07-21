7b3d5eb73166e93c0e1fdddb6247c7a4bfe9837f
JenitV <jenit>
2018-07-21 09:45:23 +0530

    Add project files.

---
+++ LocationBaseparkingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocationBaseparkingSystem.Startup))]
+namespace LocationBaseparkingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
