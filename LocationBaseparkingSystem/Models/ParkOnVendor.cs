7119df42c64e9d9e54ea2f934bdaf15a688a7a63
JenitV <jenit>
2018-07-21 10:50:14 +0530

    update in db

--- LocationBaseparkingSystem/Models/ParkOnVendor.cs
+++ LocationBaseparkingSystem/Models/ParkOnVendor.cs
@@ -7,25 +7,31 @@ namespace LocationBaseparkingSystem.Models
 {
     public class ParkOnVendor
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
 
         public Guid UserId { get; set; }
+
+        public string Email { get; set; }
+
         public string Name { get; set; }
-        public string Long { get; set; }
 
-        public decimal Lat { get; set; }
+        public string Longitude { get; set; }
 
-        public decimal Address { get; set; }
+        public decimal? Latitude { get; set; }
+
+        public decimal? Address { get; set; }
 
         public string LandMark { get; set; }
 
-        public int  NoOfParkingSpace{ get; set; }
+        public int? NoOfParkingSpace { get; set; }
+
         public DateTime CreatedDate { get; set; }
+
         public bool IsActive { get; set; }
 
-        public int HourRate { get; set; }
+        public int? HourRate { get; set; }
 
-        public int Area { get; set; }
-        public int Email { get; set; }
+        public string Area { get; set; }
+        
     }
 }
